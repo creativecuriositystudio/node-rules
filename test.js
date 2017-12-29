f113901ced79ae875541e234f6a44cfc8af1e379
Daniel Cavanagh <danielcavanagh85@gmail.com>
2017-12-29 16:42:28 +1000

    fix issue with dig not working with array indices; and $where using wrong key

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled rejection');
+  console.log(err.stack);
+});
+
 var RuleEngine = require('./lib/node-rules.js');
 
 var engine = new RuleEngine([{
-    condition: [{ length: { $gt: 7 } }, { length: { $lte: 5 }}],
+    condition: [{ length: { $lt: 7 } }, { length: { $lte: 5 }}],
     consequence: function (engine) { this.length = 1; this.matched = true; engine.stop(); },
 }]);
 
-engine.execute({ length: 5 }, function (result) {
-    console.log(result);
-});
+engine.execute({ length: 5 }).then(console.log);
