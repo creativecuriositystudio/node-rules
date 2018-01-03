c1c2ee4958e93d2bd3ce5b776019e788143d5383
Daniel Cavanagh <danielcavanagh85@gmail.com>
2018-01-03 18:51:45 +1000

    fix bugs in 703each

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ process.on('unhandledRejection', (err) => console.log(err.stack));
 var RuleEngine = require('./lib/node-rules.js');
 
 var engine = new RuleEngine([{
-    condition: [{ items: { '#min': { $lt: 4 }}}, { 'items#max': { $maxOf: { $field: 'items' } }}],
-    consequence: function (engine) { this.items = false; this.matched = true; engine.stop(); },
+    conditions: [{ items: { '#min': { $lt: 4 }}}, { 'items#max': { $maxOf: { $field: 'items' }}}],
+    consequences: [{ items: { $each: { $mul: { $field: 'item' }}}}],
 }]);
 
 engine.execute({ items: [1, 3, 5, 2], item: 4 }).then(console.log);
