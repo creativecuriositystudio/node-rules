4661d12120fb43e9c1c02ca45a3813854bfca42a
Daniel Cavanagh <danielcavanagh85@gmail.com>
2018-02-05 11:54:46 +1000

    handle missing/empty arrays in permutations properly

--- test.js
+++ test.js
@@ -3,8 +3,10 @@ process.on('unhandledRejection', (err) => console.log(err.stack));
 var RuleEngine = require('./lib/node-rules.js');
 
 var engine = new RuleEngine([{
+    name: 'test-rule',
+    priority: 0,
     conditions: [{ items: { '#min': { $lt: 4 }}}, { 'items#max': { $maxOf: { $field: 'items' }}}],
     consequences: [{ items: { $each: { $mul: { $field: 'item' }}}}],
 }]);
 
-engine.execute({ items: [1, 3, 5, 2], item: 4 }).then(console.log);
+engine.execute({ items: [1, 3, 5, 2], item: 4, index: [1, 2] }, ['index']).then(console.log);
