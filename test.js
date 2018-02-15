183d62537739a7c54c1db0211eb168a908dfa875
Daniel Cavanagh <danielcavanagh85@gmail.com>
2018-02-16 01:59:20 +1000

    add date diff calculators (days between, hours between, etc); add 'global' facts for shared data across forEach runs

--- test.js
+++ test.js
@@ -1,12 +1,30 @@
 process.on('unhandledRejection', (err) => console.log(err.stack));
 
 var RuleEngine = require('./lib/node-rules.js');
+var moment = require('moment');
 
 var engine = new RuleEngine([{
     name: 'test-rule',
     priority: 0,
-    conditions: [{ items: { '#min': { $lt: 4 }}}, { 'items#max': { $maxOf: { $field: 'items' }}}],
-    consequences: [{ items: { $each: { $mul: { $field: 'item' }}}}],
+    conditions: [{
+        items: { '#min': { $lt: 4 }},
+        'items#max': { $maxOf: { $field: 'items' }},
+        index: 2,
+    }],
+    consequences: [{
+        items: { $each: { $mul: { $field: 'multiplier' }}},
+        daysBetween: { $daysBetween: [{ $field: 'dateB' }, { $field: 'dateA' }]},
+        '_global.this': 'isGlobal',
+    }],
 }]);
 
-engine.execute({ items: [1, 3, 5, 2], item: 4, index: [1, 2] }, ['index']).then(console.log);
+var now = moment();
+
+engine.execute({
+    items: [1, 3, 5, 2],
+    multiplier: 4,
+    dateA: now.toDate(),
+    dateB: now.add(5, 'days').toISOString(),
+    daysBetween: null,
+    index: [1, 2]
+}, ['index', { this: 'isNotGlobal' }]).then(console.log);
